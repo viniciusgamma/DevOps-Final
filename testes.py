2801614f2e638cf9ebccfdb7abc24dc8b881b142
Vinicius Gama <viniciusgama694@gmail.com>
2025-06-03 20:36:39 -0300

    Mudando meus testes

--- testes.py
+++ testes.py
@@ -1,26 +1,24 @@
 import unittest
 from app import app
 
-class SimpleDifferentTests(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.client = app.test_client()
+class SimpleTests(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
 
-    def test_home_response_type(self):
-        # Verifica se o retorno da home é JSON
-        response = self.client.get('/')
-        self.assertEqual(response.content_type, 'application/json')
+    def test_home_status(self):
+        # Verifica se a home retorna 200
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
 
-    def test_items_list_length(self):
-        # Verifica se a lista de itens tem exatamente 3 elementos
-        response = self.client.get('/items')
-        items = response.get_json().get("items", [])
-        self.assertEqual(len(items), 3)
+    def test_items_status(self):
+        # Verifica se /items retorna 200
+        res = self.client.get('/items')
+        self.assertEqual(res.status_code, 200)
 
-    def test_swagger_ui_available(self):
-        # Verifica se a rota do Swagger UI está disponível (deve retornar 200 ou 302)
-        response = self.client.get('/swagger/')
-        self.assertIn(response.status_code, [200, 302])
+    def test_not_found(self):
+        # Verifica se uma rota inválida retorna 404
+        res = self.client.get('/naoexiste')
+        self.assertEqual(res.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
